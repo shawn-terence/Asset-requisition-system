b44b0262bcbda55fb69af0f1f818e3760883545a
terence <terenceshawn18@outlook.com>
2024-07-16 13:31:55 +0300

    chore:changed urls to a better convention

--- backend/urls.py
+++ backend/urls.py
@@ -21,14 +21,14 @@ from api.views import*
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("register/", UserRegistrationView.as_view(), name="register"),
-    path("login/",UserLoginView.as_view(),name="login"),
-    path("addasset/",AssetAddView.as_view(),name="add_asset"),
-    path("updatepassword/",UserUpdatePasswordView.as_view(),name="update_password"),
-    path("ChangeRole/",ChangeUserRoleView.as_view(),name="change role"),
-    path("deleteUser/",DeleteUserView.as_view(),name="delete user"),
-    path("getallusers/",AllUsersView.as_view(),name="get all users"),
-    path("getallassets/",AssetListView.as_view(),name="get all assets"),
+    path("user/register/", UserRegistrationView.as_view(), name="register"),
+    path("user/login/",UserLoginView.as_view(),name="login"),
+    path("assets/add/",AssetAddView.as_view(),name="add_asset"),
+    path("user/updatepassword/",UserUpdatePasswordView.as_view(),name="update_password"),
+    path("user/changeRole/",ChangeUserRoleView.as_view(),name="change role"),
+    path("user/deleteUser/",DeleteUserView.as_view(),name="delete user"),
+    path("user/allusers/",AllUsersView.as_view(),name="get all users"),
+    path("assets/allassets/",AssetListView.as_view(),name="get all assets"),
     path("assets/<int:asset_id>/request/",AssetUpdateView.as_view(),name="request asset"),
     path('requests/', RequestListView.as_view(), name='request-list'),
     path('requests/<int:request_id>/', RequestActionView.as_view(), name='request-action')
