378c596c894b5a657c34e851944156770d34bc1d
terence <terenceshawn18@outlook.com>
2024-07-17 22:17:12 +0300

    fix:patching issue with cors

--- backend/settings.py
+++ backend/settings.py
@@ -98,6 +98,8 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-CORS_ALLOW_ALL_ORIGINS=True
-CORS_ALLOW_CREDENTIALS = True
-
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://asset-requisition-system.onrender.com"
+]
