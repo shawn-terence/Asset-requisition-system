71a90dedeb63a950ebb56d42b3579badb32f38f9
shawn-terence <145427330+shawn-terence@users.noreply.github.com>
2024-07-19 18:11:49 +0300

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -98,8 +98,4 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "https://asset-requisition-system.onrender.com"
-]
+CORS_ALLOW_ALL_ORIGINS = True
